b73d8ba6c6d1458e1add9555f6b7983af14836a8
Pepijn Bruienne <bruienne@umich.edu>
2014-10-20 18:03:19 -0400

    Add Dockerfile for base bsdpy image, CoreOS service files.

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+/usr/local/bin/nfs-client
+sleep 2
 service nginx start
 /usr/sbin/in.tftpd -l --permissive /nbi
 cd /bsdpy
 git pull
-./bsdpserver.py -i ${DOCKER_BSDPY_IFACE} -r ${DOCKER_BSDPY_PROTO} &
+./bsdpserver.py -p ${DOCKER_BSDPY_PATH} -i ${DOCKER_BSDPY_IFACE} -r ${DOCKER_BSDPY_PROTO} &
 sleep 2
 tail -f /var/log/bsdpserver.log
