301adcf66ee1dc09b4e2294a9ff1366a5d8c0c3b
Pepijn Bruienne <bruienne@umich.edu>
2015-03-04 01:55:58 +0000

    Update copyright notice.

--- bsdpserver.py
+++ bsdpserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 ################################################################################
-# Copyright 2014 The Regents of the University of Michigan
+# Copyright 2015 The Regents of the University of Michigan
 #
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 #  use this file except in compliance with the License. You may obtain a copy of
